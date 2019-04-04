378c2971ed4870d878c9e17c84460961c4d94826
Guilherme Poleto <gpoleto@alunos.utfpr.edu.br>
2019-04-04 13:48:39 -0300

    Add files via upload

---
+++ 1006.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+	double a, b, c;
+	cin >> a >> b >> c;
+	a *=0.2; b*=0.3; c*=0.5;
+	printf("MEDIA = %.1lf\n", a+b+c);
+	return 0;
+}
